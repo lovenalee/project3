d46dc0bd0df93bceddce6083a8bf599ca4f45e8b
JimKTran <71141264+JimKTran@users.noreply.github.com>
2021-02-03 14:40:18 -0500

    Update app.py

--- Ballers/app.py
+++ Ballers/app.py
@@ -9,6 +9,7 @@ from flask import (
     request,
     redirect)
 
+# Initiate confiquration 
 app = Flask(__name__)
 
 # Initialize PyMongo to work with MongoDBs
@@ -19,16 +20,30 @@ client = pymongo.MongoClient(conn)
 db = client['Proj2_NBA_db']
 coll = db['PlayerPerGamesStats']
 
-## frontend routes
-@app.route("/")
-def main():
-    return render_template("index.html")
-
-# ## service routes
+# render a list from mongoDB into jsonified route
 @app.route("/conn")
-def firstRoute():
-    db.session.query().all()
-    return jsonify(coll)
+def pStats():
+    mylist = []
+    for item in coll.find(): 
+        mylist.append(item)
+    return jsonify(mylist)
+
+# render a list from mongoDB into HTML
+# @app.route("/conn")
+# def pStats(): 
+#     results = list(coll.find())
+#     return render_template("index1.html", pStats=results)
+
+# ## frontend routes
+# @app.route("/")
+# def main():
+#     return render_template("index.html")
+
+# # ## service routes
+# @app.route("/conn")
+# def firstRoute():
+#     db.session.query().all()
+#     return jsonify(coll)
 
 if __name__ == "__main__":
     app.run(debug=True)
