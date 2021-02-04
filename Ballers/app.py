cff9f647a54cd6a6b73e6e27ecc5abd98e0bca08
JimKTran <71141264+JimKTran@users.noreply.github.com>
2021-02-04 14:09:55 -0500

    updates

---
+++ Ballers/app.py
@@ -0,0 +1,19 @@
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+
+app = Flask(__name__)
+
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///sqlite.db"
+db = SQLAlchemy(app)
+
+print(db)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
