023ab41c7eb199aed6fdab982aec3cf0b6f98286
George Redak <george.redak91@gmail.com>
2021-02-06 12:38:15 -0500

    ballers changes

--- BallersApp/static/js/GOATapp.js
+++ BallersApp/static/js/GOATapp.js
@@ -1,7 +1,7 @@
+
 //SVG width and height values
 var svgWidth = 1200;
 var svgHeight = 600;
-
 //Margin data
 var margin = {
     top: 20,
@@ -9,7 +9,32 @@ var margin = {
     bottom: 100,
     left: 30
   };
-
-  //Declare graphing area (relative to SVG)
+//Declare graphing area (relative to SVG)
   var width = svgWidth - margin.left - margin.right;
   var height = svgHeight - margin.top - margin.bottom;
+
+
+d3.select("#goatbar1")
+.append("svg")
+.attr("width", svgWidth)
+.attr("height", svgHeight);
+
+
+
+
+
+d3.json("/api/goat").then(function(goatdata) {
+  console.log(goatdata)
+
+
+
+
+
+
+
+
+
+
+}).catch(function(error) {
+  console.log(error)
+});
