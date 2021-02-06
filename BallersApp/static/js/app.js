88bcf26a8645981455e30d33d504a91e34b1a6ec
JimKTran <71141264+JimKTran@users.noreply.github.com>
2021-02-05 20:41:24 -0500

    save

--- BallersApp/static/js/app.js
+++ BallersApp/static/js/app.js
@@ -1,4 +1,7 @@
 d3.json("/api/tStats").then(function(data) {
-    console.log(data)
-}).catch(function(error){
-    console.log(error)});
+    // console.log(data)
+}).catch(function(error) {
+    console.log(error)
+});
+
+  
