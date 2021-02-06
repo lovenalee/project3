a628b11f7bd7a09200d79a3e56a30286d247a459
JimKTran <71141264+JimKTran@users.noreply.github.com>
2021-02-06 10:24:52 -0500

    save

--- BallersApp/static/js/app.js
+++ BallersApp/static/js/app.js
@@ -1,7 +1,13 @@
 d3.json("/api/goat").then(function(data) {
-    console.log(data)
+    // console.log(data)
+
+    var year = data.map(item => item.year)
+    var teams = data.map(item => item.Team)
+    var teamPoints = data.map(item => item.TeamPoints)
+
+    
+
 }).catch(function(error) {
     console.log(error)
 });
 
-  
