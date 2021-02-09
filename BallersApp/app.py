b6237820b51aa4cf70d946d09103a758d3e88e91
JimKTran <71141264+JimKTran@users.noreply.github.com>
2021-02-08 21:20:29 -0500

    save

--- BallersApp/app.py
+++ BallersApp/app.py
@@ -48,7 +48,7 @@ class tStats(db.Model):
         return '<tStats %r>' % (self.name)
 
 @app.route("/api/tStats")
-def nba():
+def tStats():
     results1 = db.session.query(
     tStats.Team, 
     tStats.Year, 
