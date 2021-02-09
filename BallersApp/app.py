1ec262a0971e12d9c2428c381e6aa5f63ed75fdb
“lovenalee” <lovenalee225@gmail.com>
2021-02-08 19:30:35 -0500

    update db

--- BallersApp/app.py
+++ BallersApp/app.py
@@ -149,47 +149,33 @@ def goat():
     return jsonify(result_list2)
 
 class pStats(db.Model):
-    __tablename__ = 'WSData'
+    __tablename__ = 'WS_avg'
 
-    Player = db.Column(db.String(64), primary_key=True)
-    Tm = db.Column(db.String(10))
     Year = db.Column(db.Float)
-    Age = db.Column(db.Float)
     Rounded_Position = db.Column(db.Float)
-    WS = db.Column(db.Float)
+    WS|mean = db.Column(db.Float)
    
     def __repr__(self):
         return '<pStats %r>' % (self.name)
 
 @app.route("/api/ws")
 def ws():
-    results3 = db.session.query(pStats.Player, 
-    pStats.Tm, 
+    results3 = db.session.query(
     pStats.Year, 
-    pStats.Age, 
     pStats.Rounded_Position, 
-    pStats.WS).all()
+    pStats.WS|mean).all()
 
     result_list3 = []
     
     for result3 in results3:
         wsdata = {
-            "Player": result3[0],
-            "Tm": result3[1],
-            "Year": result3[2],
-            "Age": result3[3],
-            "Rounded_Position": result3[4],
-            "WS": result3[5]
+            "Year": result3[1],
+            "Rounded_Position": result3[2],
+            "WS|mean": result3[3]
         }
-
         result_list3.append(wsdata)
 
     return jsonify(result_list3)
-
-
-
-
-
 
 
 
